e0a1f5908cc9bd8d552e9a8f16a9bbb740e3668f
ELADIO-PC\Eladio <eladioalberto.02@gmail.com>
2017-05-27 19:59:13 -0600

    Cambios en el mostrar cupon

--- resources/views/pages/infocupon.blade.php
+++ resources/views/pages/infocupon.blade.php
@@ -14,12 +14,69 @@
                     <img class="img-responsive" src={{ $cupon->imgsrc}} alt="a picture">
                 </div>
                 <div class="col-md-4 well" >
-                    <h3>{{ $cupon->price }}</h3>ahorre {{ $cupon->save }} <h4><s>{{$cupon->normalprice }}</s></h4><br>
-                    <h3>Vendidos: {{ $cupon->sold }}</h3>
+
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tr>
+                                <h2>Ahorre un {{ $cupon->save }}</h2>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <h3>Precio: </h3>
+                                </td>
+                                <td>
+                                    <h3>{{ $cupon->price }}</h3>
+                                </td>
+                                <td>
+                                    <h3><s>{{$cupon->normalprice }}</s></h3><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Vendidos: </b>
+                                </td>
+                                <td>
+                                    <b>{{ $cupon->sold }}</b>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Ubicación: </b>
+                                </td>
+                                <td>
+                                    <b>{{ $cupon->city }}</b>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Vigencia: </b>
+                                </td>
+                                <td>
+                                    <b>{{ $cupon->period }}</b>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Categoria: </b>
+                                </td>
+                                <td>
+                                    <b>{{ $cupon->category }}</b>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <b>Más información: </b>
+                                </td>
+                                <td>
+                                    <a href="{{$cupon->websitesrc}}"><b>Click Aquí</b></a>
+                                </td>
+                            </tr>
+
+                        </table>
+                    </div>
 
                 </div>
-                '','save', 'city', 'period',
-    'category', 'websitesrc', 'active', 'pageid',
+                
 
                 
             </div>
